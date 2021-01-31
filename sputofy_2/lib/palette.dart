34c7d730ccc2dbf20d8388bc05c98f944f3178a4
astroxd <andreasciortino2004@gmail.com>
2021-01-31 22:09:24 +0100

    new playlist screen

--- sputofy_2/lib/palette.dart
+++ sputofy_2/lib/palette.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const mainColor = Color.fromARGB(255, 30, 30, 30);
 const secondaryColor = Color.fromARGB(255, 53, 53, 53);
+const thirdColor = Color.fromARGB(255, 196, 196, 196);
 const accentColor = Color.fromARGB(255, 255, 100, 0);
--- sputofy_2/lib/playlistScreen.dart
+++ sputofy_2/lib/playlistScreen.dart
@@ -2,13 +2,16 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sputofy_2/app_icons.dart';
+import 'package:sputofy_2/main.dart';
 import 'package:sputofy_2/miniPlayer.dart';
 import 'package:sputofy_2/model/audioPlayer.dart';
+import 'package:sputofy_2/palette.dart';
 
-class PlaylistScreen extends StatelessWidget {
+class PlaylistScreen1 extends StatelessWidget {
   final int index;
 
-  PlaylistScreen(this.index);
+  PlaylistScreen1(this.index);
 
   @override
   Widget build(BuildContext context) {
@@ -310,6 +313,270 @@ class PlaylistScreen extends StatelessWidget {
               showMiniPlayer(context);
             },
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class PlaylistScreen extends StatelessWidget {
+  final int index;
+
+  PlaylistScreen(this.index);
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double widthScreen = mediaQueryData.size.width;
+    return Scaffold(
+      body: Container(
+        width: widthScreen,
+        child: Column(
+          children: <Widget>[
+            _buildWidgetPlaylistInfo(widthScreen),
+            _buildWidgetPlaylistMusic()
+          ],
+        ),
+        color: mainColor,
+      ),
+    );
+  }
+
+  Widget _buildWidgetPlaylistInfo(double widthScreen) {
+    return Container(
+      width: widthScreen,
+      color: secondaryColor,
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(Icons.arrow_back_ios),
+                  Icon(
+                    Icons.more_vert,
+                    color: accentColor,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 32.0, top: 16.0, right: 32.0),
+            child: Row(
+              // mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  width: 140,
+                  height: 140,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16.0),
+                    child: Image.asset('cover.jpeg'),
+                  ),
+                ),
+                SizedBox(
+                  width: 16.0,
+                ),
+                Container(
+                  height: 140,
+                  width: 90,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        height: 108,
+                        child: Text(
+                          "Playlistdadaadawdadawdddadawddwadawd",
+                          style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                              color: accentColor),
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: true,
+                          maxLines: 3,
+                        ),
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            GestureDetector(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: mainColor,
+                                    borderRadius: BorderRadius.circular(12.0)),
+                                child: Icon(
+                                  Icons.play_arrow,
+                                  size: 32.0,
+                                  color: accentColor,
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: mainColor,
+                                    borderRadius: BorderRadius.circular(12.0)),
+                                child: Icon(
+                                  Icons.add,
+                                  size: 32.0,
+                                  color: accentColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Container(
+            width: widthScreen,
+            child: Stack(
+              children: [
+                Column(
+                  children: <Widget>[
+                    Container(
+                      height: 36,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 2.0),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 36,
+                      width: widthScreen,
+                      color: mainColor,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16.0, top: 16.0),
+                        child: Text(
+                          "13 songs, 1.32 hours",
+                          style: TextStyle(fontSize: 16.0, color: accentColor),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 32.0),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          child: Container(
+                            padding: const EdgeInsets.all(12.0),
+                            decoration: BoxDecoration(
+                                color: thirdColor,
+                                borderRadius: BorderRadius.circular(64.0)),
+                            child: Icon(
+                              Icons.repeat,
+                              size: 32.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        GestureDetector(
+                          child: Container(
+                            padding: const EdgeInsets.all(20.0),
+                            decoration: BoxDecoration(
+                                color: accentColor,
+                                borderRadius: BorderRadius.circular(64.0)),
+                            child: Icon(
+                              AppIcon.shuffle,
+                              size: 32.0,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildWidgetPlaylistMusic() {
+    return Consumer<MyAudio>(
+      builder: (context, audioPlayer, child) => Expanded(
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          itemBuilder: (context, index) {
+            Music song = audioPlayer.songList[index];
+            int durationMinute =
+                song.durationSecond >= 60 ? song.durationSecond ~/ 60 : 0;
+            int durationSecond = song.durationSecond >= 60
+                ? song.durationSecond % 60
+                : song.durationSecond;
+            String strDuration = "$durationMinute:" +
+                (durationSecond < 10 ? "0$durationSecond" : "$durationSecond");
+
+            return GestureDetector(
+              onTap: () {
+                if (index != audioPlayer.indexSongSelected) {
+                  audioPlayer.playSong(index);
+                } else if (index == audioPlayer.indexSongSelected) {
+                  audioPlayer.resumeSong();
+                }
+                showMiniPlayer(context);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.black),
+                  ),
+                ),
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            song.title,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: audioPlayer.indexSongSelected == index
+                                  ? accentColor
+                                  : Colors.black,
+                            ),
+                          ),
+                          Text(
+                            '${song.artist}',
+                            style: TextStyle(color: thirdColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text("$strDuration"),
+                    Icon(
+                      Icons.more_vert,
+                      color: accentColor,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          itemCount: audioPlayer.songList.length,
         ),
       ),
     );
