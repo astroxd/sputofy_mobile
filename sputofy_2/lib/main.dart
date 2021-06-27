b4f121b3ea377c8fd157a89274f78407230d81f3
astroxd <andreasciortino2004@gmail.com>
2021-06-27 22:44:46 +0200

    flutter create

--- sputofy_2/lib/main.dart
+++ sputofy_2/lib/main.dart
@@ -1,325 +1,113 @@
-import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
-
-import 'package:rxdart/rxdart.dart';
-
-import 'package:sputofy_2/model/SongModel.dart';
-
-import 'package:sputofy_2/pages/FouthPage.dart';
-import 'package:sputofy_2/pages/MiniPlayerPage.dart';
-import 'package:sputofy_2/pages/PlaylistListPage.dart';
-import 'package:provider/provider.dart';
-import 'package:sputofy_2/pages/SongListPage.dart';
-import 'package:sputofy_2/provider/provider.dart';
-import 'package:sputofy_2/utils/AudioPlayer.dart';
-import 'package:sputofy_2/utils/Database.dart';
-import 'package:sputofy_2/utils/palette.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => DBProvider()),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primarySwatch: Colors.orange,
-            bottomSheetTheme: BottomSheetThemeData(backgroundColor: mainColor)),
-        title: "Test",
-        home: AudioServiceWidget(
-          child: DefaultTabController(length: 4, child: MainScreen()),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-_backgroundTaskEntryPoint() {
-  print("entrypoint");
-  AudioServiceBackground.run(() => AudioPlayerTask());
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class MainScreen extends StatefulWidget {
-  @override
-  _MainScreenState createState() => _MainScreenState();
-}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-class _MainScreenState extends State<MainScreen> {
-  @override
-  void initState() {
-    start();
-    super.initState();
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    DBHelper _database = DBHelper();
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
-      backgroundColor: mainColor,
       appBar: AppBar(
-        title: Text("test"),
-        bottom: TabBar(
-          tabs: [
-            Tab(
-              icon: Icon(
-                Icons.home,
-              ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.playlist_add,
-              ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.playlist_add,
-              ),
-            ),
-            Tab(
-              icon: Icon(
-                Icons.playlist_add,
-              ),
-            ),
-          ],
-        ),
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: TabBarView(
-        children: [
-          PrimaPagina(context),
-          SongList(),
-          PlaylistsList(),
-          FourthPage(),
-        ],
-      ),
-      // bottomSheet: MiniPlayer(),
-    );
-  }
-
-  //* AudioService lo usi per parlare con la background task
-
-  start() async {
-    // final mediaList = [];
-    // for (var song in playlist) {
-    //   print(song);
-    //   final mediaItem = MediaItem(
-    //     id: song.id,
-    //     album: song.album,
-    //     title: song.title,
-    //     duration: song.duration,
-    //     artUri: song.artUri,
-    //   );
-    //   mediaList.add(mediaItem.toJson());
-    // }
-    // if (mediaList.isEmpty) return;
-    // final params = {'data': mediaList};
-    AudioService.start(
-        backgroundTaskEntrypoint: _backgroundTaskEntryPoint,
-        // params: ,
-        // androidEnableQueue: true,
-        androidNotificationColor: 0x0000ff);
-  }
-}
-
-class QueueState {
-  final List<MediaItem>? queue;
-  final MediaItem? mediaItem;
-
-  QueueState(this.queue, this.mediaItem);
-}
-
-class PlayingMediaItem {
-  final MediaItem? mediaItem;
-  final Duration position;
-  final PlaybackState playbackState;
-
-  PlayingMediaItem(this.mediaItem, this.position, this.playbackState);
-}
-
-class PrimaPagina extends StatelessWidget {
-  PrimaPagina(this.context);
-  final BuildContext context;
-
-  final List<Song> playlist = [
-    Song(
-      null,
-      '/data/user/0/com.example.sputofy_2/cache/file_picker/「Rock」 Hatsuki Yura (葉月ゆら) - 少女と黄金竜の物語 (Shoujo to Ougon Ryuu no Monogatari).mp3',
-      'oregairu',
-      'tanta',
-      "https://media.wnyc.org/i/1400/1400/l/80/1/ScienceFriday_WNYCStudios_1400.jpg",
-      Duration(milliseconds: 273057),
-    ),
-  ];
-
-  Stream<QueueState> get _queueStateStream =>
-      Rx.combineLatest2<List<MediaItem>?, MediaItem?, QueueState>(
-          AudioService.queueStream,
-          AudioService.currentMediaItemStream,
-          (queue, mediaItem) => QueueState(queue, mediaItem));
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            StreamBuilder<PlaybackState>(
-              stream: AudioService.playbackStateStream,
-              builder: (context, snapshot) {
-                final playing = snapshot.data?.playing ?? false;
-                final processingState = snapshot.data?.processingState ??
-                    AudioProcessingState.stopped;
-                print("playing var = $playing");
-                print("processingstate var = $processingState");
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    if (playing)
-                      MaterialButton(
-                        child: Text("Pause"),
-                        onPressed: pause,
-                        color: Colors.green,
-                      )
-                    else
-                      MaterialButton(
-                        child: Text("Play"),
-                        onPressed: () {
-                          // showDialogWindow(context);
-                          play();
-                        },
-                        color: Colors.green,
-                      ),
-                    if (processingState != AudioProcessingState.stopped &&
-                        processingState != AudioProcessingState.none)
-                      MaterialButton(
-                        child: Text("Stop"),
-                        onPressed: stop,
-                        color: Colors.red,
-                      ),
-                  ],
-                );
-              },
+            Text(
+              'You have pushed the button this many times:',
             ),
-            MaterialButton(
-                child: Text("ADD"),
-                color: Colors.blue,
-                onPressed: () {
-                  openPlaylist();
-                }),
-            StreamBuilder(
-              stream: _queueStateStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  QueueState queueStateStream = snapshot.data as QueueState;
-                  return Container(
-                    width: double.infinity,
-                    height: 300,
-                    child: ListView.builder(
-                      itemCount: queueStateStream.queue?.length,
-                      itemBuilder: (context, index) {
-                        final mediaItem = queueStateStream.queue![index];
-                        final currentMediaItem = queueStateStream.mediaItem;
-                        return MaterialButton(
-                          onPressed: () => seek(mediaItem.id),
-                          child: mediaItem == currentMediaItem
-                              ? Text(
-                                  "${currentMediaItem?.title}---${currentMediaItem?.duration.toString()}",
-                                  style: TextStyle(color: Colors.blue),
-                                )
-                              : Text(
-                                  "${mediaItem.title}----${mediaItem.duration.toString()}"),
-                        );
-                      },
-                    ),
-                  );
-                } else {
-                  return CircularProgressIndicator();
-                }
-              },
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  start() async {
-    // final mediaList = [];
-    // for (var song in playlist) {
-    //   print(song);
-    //   final mediaItem = MediaItem(
-    //     id: song.id,
-    //     album: song.album,
-    //     title: song.title,
-    //     duration: song.duration,
-    //     artUri: song.artUri,
-    //   );
-    //   mediaList.add(mediaItem.toJson());
-    // }
-    // if (mediaList.isEmpty) return;
-    // final params = {'data': mediaList};
-    AudioService.start(
-        backgroundTaskEntrypoint: _backgroundTaskEntryPoint,
-        // params: params,
-        androidEnableQueue: true,
-        androidNotificationColor: 0x0000ff);
-  }
-
-  openPlaylist() {
-    final mediaList = [];
-    for (var song in playlist) {
-      print(song);
-      final mediaItem = MediaItem(
-        id: song.path!,
-        album: song.author!,
-        title: song.title!,
-        duration: song.duration,
-        artUri: Uri.parse(song.cover!),
-      );
-      mediaList.add(mediaItem.toJson());
-    }
-    if (mediaList.isEmpty) return;
-    // final params = {'data': mediaList};
-    AudioService.customAction('openPlaylist', mediaList);
-    // Navigator.push(
-    //     context,
-    //     MaterialPageRoute(
-    //       builder: (context) => PlaylistScreen(),
-    //     ));
-  }
-
-  pause() {
-    print("Pause");
-    AudioService.pause();
-  }
-
-  stop() {
-    print("stop");
-    AudioService.stop();
-  }
-
-  play() async {
-    if (AudioService.running) {
-      AudioService.play();
-    } else {
-      start();
-    }
-  }
-
-  add(MediaItem mediaItem) {
-    AudioService.addQueueItem(mediaItem);
-    // AudioService.setRating(Rating.newHeartRating(true));
-  }
-
-  seek(String mediaId) {
-    AudioService.skipToQueueItem(mediaId);
   }
 }
