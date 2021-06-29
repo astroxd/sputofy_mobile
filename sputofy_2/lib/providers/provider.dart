0d3621dfbbdaa588da137772bafcc8da80696045
astroxd <andreasciortino2004@gmail.com>
2021-06-29 17:25:19 +0200

    first part of playlistListPage ui

--- sputofy_2/lib/providers/provider.dart
+++ sputofy_2/lib/providers/provider.dart
@@ -10,8 +10,8 @@ class DBProvider extends ChangeNotifier {
   Future<List<Song>>? _playlistSongs;
   Future<List<Song>>? get playlistSongs => _playlistSongs;
 
-  Future<List<Playlist>>? _playlists;
-  Future<List<Playlist>>? get playlists => _playlists;
+  List<Playlist> _playlists = [];
+  List<Playlist> get playlists => _playlists;
   bool isAlphabeticalOrder = false;
 
   List<Song> _songs = [];
@@ -20,11 +20,13 @@ class DBProvider extends ChangeNotifier {
   DBProvider() {
     _database = DBHelper();
     getSongs();
+    getPlaylists();
   }
 
-  Future<void> getPlaylistSongs(int playlistID) async {
+  Future<List<Song>> getPlaylistSongs(int playlistID) async {
     _playlistSongs = _database.getPlaylistSongs(playlistID);
     // notifyListeners();
+    return await _database.getPlaylistSongs(playlistID);
   }
 
   Future<void> savePlaylistSongs(
@@ -41,15 +43,17 @@ class DBProvider extends ChangeNotifier {
   }
 
   Future<void> getPlaylists() async {
-    Future<List<Playlist>> playlists = _database.getPlaylists();
+    List<Playlist> playlists = await _database.getPlaylists();
+
     //*sortDirection
     if (isAlphabeticalOrder) {
-      playlists.then((value) => value.sort((a, b) => a.name.compareTo(b.name)));
+      playlists.sort((a, b) => a.name.compareTo(b.name));
     } else {
-      playlists.then((value) => value.sort((a, b) => b.name.compareTo(a.name)));
+      playlists.sort((a, b) => b.name.compareTo(a.name));
     }
 
     _playlists = playlists;
+    notifyListeners();
   }
 
   void sortPlaylists() {
@@ -59,12 +63,14 @@ class DBProvider extends ChangeNotifier {
 
   Future<void> savePlaylist(Playlist playlist) async {
     _database.savePlaylist(playlist);
+    getPlaylists();
     notifyListeners();
   }
 
   Future<void> deletePlaylist(int playlistID) async {
     _database.deleteAllPlaylistSongs(playlistID);
     _database.deletePlaylist(playlistID);
+    getPlaylists();
     notifyListeners();
   }
 
--- sputofy_2/lib/screens/playlistListScreen/playlist_list_screen.dart
+++ sputofy_2/lib/screens/playlistListScreen/playlist_list_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sputofy_2/models/playlist_model.dart';
+import 'package:sputofy_2/models/song_model.dart';
+import 'package:sputofy_2/providers/provider.dart';
+import 'package:sputofy_2/theme/palette.dart';
 
 class PlaylistListScreen extends StatefulWidget {
   const PlaylistListScreen({Key? key}) : super(key: key);
@@ -10,14 +15,86 @@ class PlaylistListScreen extends StatefulWidget {
 class _PlaylistListScreenState extends State<PlaylistListScreen> {
   @override
   Widget build(BuildContext context) {
+    final playlists = context.watch<DBProvider>().playlists;
+    print(playlists);
+
     return Scaffold(
       body: Column(
         children: [
+          MaterialButton(
+            onPressed: () => Provider.of<DBProvider>(context, listen: false)
+                .savePlaylist(Playlist(
+                    null,
+                    "namaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaae",
+                    "cover",
+                    DateTime.now())),
+            child: Text(
+              "Create Playlist",
+              style: TextStyle(color: kAccentColor),
+            ),
+          ),
           Expanded(
             child: ListView.builder(
-              itemCount: 3,
+              itemCount: playlists.length,
               itemBuilder: (context, index) {
-                return Container();
+                Playlist playlist = playlists[index];
+                Future<List<Song>> playlistSongs =
+                    context.watch<DBProvider>().getPlaylistSongs(playlist.id!);
+                return Container(
+                  // padding: const EdgeInsets.only(
+                  //     top: 8.0, bottom: 8.0, right: 8.0, left: 16.0),
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        )
+                      ],
+                      borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10.0),
+                            bottomLeft: Radius.circular(10.0)),
+                        child: Image.asset(
+                          'cover.jpeg',
+                          width: 50.0,
+                          height: 50.0,
+                        ),
+                      ),
+                      SizedBox(width: 16.0),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              playlist.name,
+                              style: Theme.of(context).textTheme.subtitle1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            FutureBuilder<List<Song>>(
+                                future: playlistSongs,
+                                builder: (context, snapshot) {
+                                  List<Song> songs = snapshot.data ?? [];
+
+                                  return Text("${songs.length}",
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .subtitle2);
+                                }),
+                          ],
+                        ),
+                      ),
+                      GestureDetector(
+                          onTap: null, child: Icon(Icons.more_vert)),
+                    ],
+                  ),
+                );
               },
             ),
           ),
